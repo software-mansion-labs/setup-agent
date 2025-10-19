2335256a4bd2f76d8bab8819492f884b66862c45
mateuszlampert <mateuszlampert@gmail.com>
2025-10-20 01:10:19 +0200

    chore: move shell_registry to shell module'

--- src/shell/__init__.py
+++ src/shell/__init__.py
@@ -2,5 +2,6 @@ from shell.interactive_shell import InteractiveShell
 from shell.safe_interactive_shell.shell import SafeInteractiveShell
 from shell.base_shell import BaseShell
 from shell.types import StreamToShellOutput
+from shell.shell_registry import ShellRegistry
 
-__all__ = ["BaseShell", "InteractiveShell", "SafeInteractiveShell", "StreamToShellOutput"]
+__all__ = ["ShellRegistry", "BaseShell", "InteractiveShell", "SafeInteractiveShell", "StreamToShellOutput"]
