22f7233ae9c862c06bb07228f2c3dda76930b358
mateuszlampert <mateuszlampert@gmail.com>
2025-10-19 22:14:53 +0200

    feat: add guidelines retriever to the flow

--- src/main.py
+++ src/main.py
@@ -1,28 +1,34 @@
 from langchain_core.messages import HumanMessage
 from langgraph.graph import StateGraph, START, END
 from agents.runner import runner
-from src.graph_state import GraphState
-from agents.agents import AgentNode
+from graph_state import GraphState
+from nodes.nodes import WorkflowNode
 from agents.planner import planner
 from agents.installer import installer
+from nodes.guidelines_retriever import GuidelinesRetrieverNode
 from dotenv import load_dotenv
 from shell.safe_interactive_shell import get_safe_interactive_shell
-from src.nodes.guidelines_retriever import GuidelinesRetrieverNode
-
+from nodes.guidelines_retriever import GuidelinesRetrieverNode
+from config import Config
 
 # TODO: update workflow by adding condtional reverse edges
 def main():
     load_dotenv()
 
-    graph = StateGraph(GraphState)
-    graph.add_node(AgentNode.PLANNER, planner)
-    graph.add_node(AgentNode.INSTALLER, installer)
-    graph.add_node(AgentNode.RUNNER, runner)
+    config = Config.init(".")
+    guidelines_retriever_node = GuidelinesRetrieverNode()
 
-    graph.add_edge(START, AgentNode.PLANNER)
-    graph.add_edge(AgentNode.PLANNER, AgentNode.INSTALLER)
-    graph.add_edge(AgentNode.INSTALLER, AgentNode.RUNNER)
-    graph.add_edge(AgentNode.RUNNER, END)
+    graph = StateGraph(GraphState)
+    graph.add_node(WorkflowNode.GUIDELINES_RETRIEVER_NODE.value, guidelines_retriever_node.invoke)
+    graph.add_node(WorkflowNode.PLANNER.value, planner)
+    graph.add_node(WorkflowNode.INSTALLER.value, installer)
+    graph.add_node(WorkflowNode.RUNNER.value, runner)
+
+    graph.add_edge(START, WorkflowNode.GUIDELINES_RETRIEVER_NODE.value)
+    graph.add_edge(WorkflowNode.GUIDELINES_RETRIEVER_NODE.value, WorkflowNode.PLANNER.value)
+    graph.add_edge(WorkflowNode.PLANNER.value, WorkflowNode.INSTALLER.value)
+    graph.add_edge(WorkflowNode.INSTALLER.value, WorkflowNode.RUNNER.value)
+    graph.add_edge(WorkflowNode.RUNNER.value, END)
 
     workflow = graph.compile()
 
@@ -33,11 +39,8 @@ def main():
                     content="Install all required tools according to the provided guidelines."
                 )
             ]
-        } # type: ignore
+        }  # type: ignore
     )
-
-    interactive_shell = get_safe_interactive_shell()
-    interactive_shell.run_command("ls -al")
 
 if __name__ == "__main__":
     main()
