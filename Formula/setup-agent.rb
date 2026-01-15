8a297081eab7fd496894fca3b461dc711ca3d702
mateuszlampert <mateuszlampert@gmail.com>
2026-01-15 14:11:55 +0100

    chore: update url for brew

--- Formula/setup-agent.rb
+++ Formula/setup-agent.rb
@@ -3,7 +3,7 @@ class SetupAgent < Formula
 
   desc "AI agent for installing tools and running projects."
   homepage "https://github.com/software-mansion-labs/setup-agent"
-  url "https://github.com/software-mansion-labs/setup-agent/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/software-mansion-labs/setup-agent/archive/refs/tags/v0.2.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
